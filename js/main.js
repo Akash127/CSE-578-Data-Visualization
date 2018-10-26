f29684e5b59e6d82f661d16c504941f5cb202326
Akash127 <mail.akashsharma@gmail.com>
2018-10-26 14:39:33 -0700

    added description table

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 
 
+var scatterPlot, desc;
+var selectedPoint;
+
+function pointSelected(event) {
+  selectedPoint = event;
+  desc.update()
+}
+
 // Load Dataset and Charts
 d3.csv("dataset/cleaned_matches_dataset.csv").then(function(data) {
   scatterPlot = new ScatterPlot(data, "#chart-area1");
+  if(!selectedPoint) {
+    selectedPoint = data[0]
+  }
+  desc = new DescriptionTable("#desc");
 });
-
