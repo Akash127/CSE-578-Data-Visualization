4094dbdd38dc8711e24d7be51dcb242a025b5c64
Akash127 <mail.akashsharma@gmail.com>
2018-10-28 00:33:34 -0700

    add minimal point drag functionality

--- js/scatterPlot.js
+++ js/scatterPlot.js
@@ -112,4 +112,20 @@ ScatterPlot.prototype.drawvis = function() {
       // .on("mouseover", tip.show)
       .on("mouseout", tip.hide)
       .attr("fill", "#FFB55F")
+      .call(d3.drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended));
+}
+
+function dragstarted(d) {
+  d3.select(this).raise().classed("active", true);
+}
+
+function dragged(d) {
+  d3.select(this).attr("cx", d.x = d3.event.x).attr("cy", d.y = d3.event.y);
+}
+
+function dragended(d) {
+  d3.select(this).classed("active", false);
 }
