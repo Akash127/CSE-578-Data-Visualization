ccb1c32a81fd6b964cbdabd248cd8449e14de500
utkagarwal <utkag789@gmail.com>
2018-10-31 15:33:07 -0700

    akash k changes

--- js/scatterPlot.js
+++ js/scatterPlot.js
@@ -1,6 +1,12 @@
 var initX=9;
 var initY=13;
 
+var lineData = [
+  {"x": 0, "y": -Number.MAX_SAFE_INTEGER},
+  {"x": 0, "y": Number.MAX_SAFE_INTEGER}
+] 
+
+
 ScatterPlot = function(data, containerClassName, xv, yv) {
   this.data = data
   this.containerClassName = containerClassName
@@ -11,55 +17,92 @@ ScatterPlot = function(data, containerClassName, xv, yv) {
 
 ScatterPlot.prototype.init = function() {
   var vis = this;
-  vis.margin = {left:50, right:10, top:10, bottom:70};
+  vis.margin = {left:50, right:50, top:50, bottom:50};
 
-  vis.width = 500 - vis.margin.left - vis.margin.right;
-  vis.height = 550 - vis.margin.top - vis.margin.bottom;
+  vis.width = 900 - vis.margin.left - vis.margin.right;
+  vis.height = 700 - vis.margin.top - vis.margin.bottom;
 
-  vis.svgCanvas = d3.select(vis.containerClassName).append("svg")
+  vis.svg = d3.select(vis.containerClassName).append("svg")
     .attr("width", vis.width + vis.margin.left + vis.margin.right)
     .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
 
-  vis.scatterPlotGroup = vis.svgCanvas.append("g")
+  vis.scatterPlotGroup = vis.svg.append("g")
     .attr("transform", "translate(" + vis.margin.left + "," + vis.margin.top + ")")
-    var columns=this.data.columns;
-    var max=d3.max(this.data,function(d){return +d["coord"][columns[initX]]});
-    var min =d3.min(this.data,function(d){return +d["coord"][columns[initX]]});
-    var ymax=d3.max(this.data,function(d){return +d["coord"][columns[initY]]});
-    var ymin =d3.min(this.data,function(d){return +d["coord"][columns[initY]]});
-   // console.log(columns);
+    
+  vis.columns = this.data.columns;
+
+  var xmax = d3.max(this.data,function(d){return +d["coord"][vis.columns[initX]]});
+  var xmin = d3.min(this.data,function(d){return +d["coord"][vis.columns[initX]]});
+  var ymax = d3.max(this.data,function(d){return +d["coord"][vis.columns[initY]]});
+  var ymin = d3.min(this.data,function(d){return +d["coord"][vis.columns[initY]]});
+
   // Initiating Scales for Axis and Inverted Axis
 
-  vis.xScale_scatter = d3.scaleLinear()
+  vis.xScale = d3.scaleLinear()
     .range([0, vis.width])
-    .domain([min,max])
+    .domain([xmin,xmax])
 
-  vis.yScale_scatter = d3.scaleLinear()
-    .range([0, vis.height])
-    .domain([ymin, ymax])
-  
-    vis.invertedYScale_scatter = d3.scaleLinear()
+  vis.yScale = d3.scaleLinear()
     .range([vis.height, 0])
     .domain([ymin, ymax])
   
   
   // Creating and Adding Axis
-  vis.xAxis_scatter = d3.axisBottom(vis.xScale_scatter)
-    .ticks(4)
-  vis.xAxis = vis.scatterPlotGroup.append("g")
-    .attr("class", "x-axis")
-    .attr("transform", "translate(0," + vis.height + ")")
-    .call(vis.xAxis_scatter);
+  // create Axis Objects
+  vis.xAxis = d3.axisBottom(vis.xScale)
+    .ticks(20)
+    .tickSize(-vis.height)
+  
+  vis.yAxis = d3.axisLeft(vis.yScale)
+    .ticks(20)
+    .tickSize(-vis.width)
+    
+  // Draw Axis
+  vis.gX = vis.svg.append('g')
+    .attr('transform', 'translate(' + vis.margin.left + ',' + (vis.margin.top + vis.height) + ')')
+    .call(vis.xAxis);
+  
+  vis.gY = vis.svg.append('g')
+    .attr('transform', 'translate(' + vis.margin.left + ',' + vis.margin.top + ')')
+    .call(vis.yAxis);
 
-  vis.yAxis_scatter = d3.axisLeft(vis.invertedYScale_scatter)
-   // .ticks(4)
-  vis.yAxis = vis.scatterPlotGroup.append("g")
-    .attr("class", "y-axis")
-    .call(vis.yAxis_scatter);
+  // Define the Clipping Area
+  vis.svg.append("defs").append("clipPath")
+    .attr("id", "clip")
+    .append("rect")
+      .attr("width", vis.width)
+      .attr("height", vis.height)
+
+
+  // Define Zoom Object
+  var zoom = d3.zoom()
+    .scaleExtent([0, 5])
+    // .extent([0, 0], [vis.width, vis.height])
+    .on("zoom", function() {
+      return zoomed(vis);
+    });
+
+  // Define Listener Element for Zoom
+  vis.svg.append("rect")
+    .attr("width", vis.width)
+    .attr("height", vis.height)
+    .style("fill", "none")
+    .style("pointer-events", "all")
+    .attr('transform', 'translate(' + vis.margin.left + ',' + vis.margin.top + ')')
+    .call(zoom)
+
+  // Define Origin Lines
+  vis.yOrigin = d3.line()
+    .x(function(d) {return vis.xScale(d.x)})
+    .y(function(d) {return vis.yScale(d.y)})
+
+  vis.xOrigin = d3.line()
+    .x(function(d) {return vis.xScale(d.y)})
+    .y(function(d) {return vis.yScale(d.x)})
   
   // Adding Axis Labels
   vis.scatterPlotGroup.append("text")
-    .text(columns[initX])
+    .text(vis.columns[initX])
     .attr("y", -35)
     .attr("x", -380)
     .attr("class", "y-axis-label")
@@ -68,7 +111,7 @@ ScatterPlot.prototype.init = function() {
 
 
   vis.scatterPlotGroup.append("text")
-    .text(columns[initY])
+    .text(vis.columns[initY])
     .attr("y", vis.height + 50)
     .attr("x", 150)
     .attr("class", "x-axis-label")
@@ -95,42 +138,89 @@ ScatterPlot.prototype.drawvis = function() {
 
   var vis = this;
 
-  // Tooltip
-  var tip = d3.tip().attr('class', 'd3-tip')
-    .html(function(d) {
-      var text = "<strong><span style='color: #E57255'>"+d.player1+" defeated "+d.player2+"("+d.year+")</span></strong><br><br>"
-      text += "First Point Won-("+ d.player1 + "): " + d.firstPointWon1 + "%<br>"
-      text += "Return Point-(" + d.player2 + "): " + d.return2 + "%<br>"
-      return text;
-    });
-  vis.scatterPlotGroup.call(tip);
+  // // Tooltip
+  // var tip = d3.tip().attr('class', 'd3-tip')
+  //   .html(function(d) {
+  //     var text = "<strong><span style='color: #E57255'>"+d.player1+" defeated "+d.player2+"("+d.year+")</span></strong><br><br>"
+  //     text += "First Point Won-("+ d.player1 + "): " + d.firstPointWon1 + "%<br>"
+  //     text += "Return Point-(" + d.player2 + "): " + d.return2 + "%<br>"
+  //     return text;
+  //   });
+  // vis.scatterPlotGroup.call(tip);
 
   // Adding Circles
   vis.circles = vis.scatterPlotGroup.selectAll("circle")
     .data(vis.data)
-    var columns=this.data.columns;
+  
   vis.circles.enter()
     .append("circle")
       .attr("cx", function(d, i){
-        return vis.xScale_scatter(+d["coord"][columns[initY]]);
+        return vis.xScale(+d["coord"][vis.columns[initY]]);
       })
       .attr("cy", function(d) {
-        return vis.height - vis.yScale_scatter(+d["coord"][columns[initX]]);
+        return vis.yScale(+d["coord"][vis.columns[initX]]);
       })
       .attr("r", 5)
+      .attr("clip-path", "url(#clip)")
       .on("mouseover", pointSelected)
       // .on("mouseover", tip.show)
-      .on("mouseout", tip.hide)
+      // .on("mouseout", tip.hide)
       .attr("fill", "#FFB55F")
       .call(d3.drag()
         .on("start", dragstarted)
         .on("drag", dragged)
         .on("end", dragended));
+
+  // Adding X Origin
+  vis.scatterPlotGroup.append("path")
+    .attr("d", vis.yOrigin(lineData))
+    .attr("class", "yOriginLine")
+    .attr("clip-path", "url(#clip)")
+  // Adding Y Origin
+  vis.scatterPlotGroup.append("path")
+    .attr("d", vis.xOrigin(lineData))
+    .attr("class", "xOriginLine")
+    .attr("clip-path", "url(#clip)")
+
 }
+
+
+function zoomed (vis) {
+
+  // create new scale ojects based on event
+  var new_xScale = d3.event.transform.rescaleX(vis.xScale);
+  var new_yScale = d3.event.transform.rescaleY(vis.yScale);
+  // Update axes
+  vis.gX.call(vis.xAxis.scale(new_xScale));
+  vis.gY.call(vis.yAxis.scale(new_yScale));
+
+  vis.scatterPlotGroup.selectAll("circle").data(vis.data)
+   .attr('cx', function(d) {return new_xScale(+d["coord"][vis.columns[initY]])})
+   .attr('cy', function(d) {return new_yScale(+d["coord"][vis.columns[initX]])});
+
+  vis.xOrigin = d3.line()
+    .x(function(d) {return new_xScale(d.y)})
+    .y(function(d) {return new_yScale(d.x)})
+  
+  vis.scatterPlotGroup.select(".xOriginLine")
+    .attr("d", vis.xOrigin(lineData))
+    .attr("clip-path", "url(#clip)")
+
+  vis.yOrigin = d3.line()
+   .x(function(d) {return new_xScale(d.x)})
+   .y(function(d) {return new_yScale(d.y)})
+  
+  vis.scatterPlotGroup.select(".yOriginLine")
+    .attr("d", vis.yOrigin(lineData))
+    .attr("clip-path", "url(#clip)")
+}
+
+
 function clone(selector) {
   var node = d3.select(selector).node();
   return d3.select(node.parentNode.insertBefore(node.cloneNode(true), node.nextSibling));
 }
+
 function dragstarted(d) {
   console.log(d);
   //console.log(this);
